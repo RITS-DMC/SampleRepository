1ab92b7f2da98c970c22eceff4d77f79d6c3f562
RITS-DMC <dmc@ritsconsulting.com>
2023-12-28 13:22:27 +0530

    sample commit

---
+++ exmpleplugin/webapp/exmpleplugin/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/dm/dme/podfoundation/component/production/ProductionUIComponent",
+	"sap/ui/Device"
+], function (ProductionUIComponent, Device) {
+	"use strict";
+
+	return ProductionUIComponent.extend("rits.custom.plugin.exmpleplugin.exmpleplugin.Component", {
+		metadata: {
+			manifest: "json"
+		}
+	});
+});
---
+++ exmpleplugin/webapp/exmpleplugin/builder/PropertyEditor.js
@@ -0,0 +1,43 @@
+sap.ui.define([
+    "sap/ui/model/resource/ResourceModel",
+    "sap/dm/dme/podfoundation/control/PropertyEditor"
+], function (ResourceModel, PropertyEditor) {
+    "use strict";
+    
+    var oFormContainer;
+
+    return PropertyEditor.extend( "rits.custom.plugin.exmpleplugin.exmpleplugin.builder.PropertyEditor" ,{
+
+		constructor: function(sId, mSettings){
+			PropertyEditor.apply(this, arguments);
+			
+			this.setI18nKeyPrefix("customComponentListConfig.");
+			this.setResourceBundleName("rits.custom.plugin.exmpleplugin.exmpleplugin.i18n.builder");
+			this.setPluginResourceBundleName("rits.custom.plugin.exmpleplugin.exmpleplugin.i18n.i18n");
+		},
+		
+		addPropertyEditorContent: function(oPropertyFormContainer){
+			var oData = this.getPropertyData();
+			
+			this.addSwitch(oPropertyFormContainer, "backButtonVisible", oData);
+			this.addSwitch(oPropertyFormContainer, "closeButtonVisible", oData);
+						
+			this.addInputField(oPropertyFormContainer, "title", oData);
+			this.addInputField(oPropertyFormContainer, "text", oData);
+
+            oFormContainer = oPropertyFormContainer;
+		},
+		
+		getDefaultPropertyData: function(){
+			return {
+				
+				"backButtonVisible": true,
+				"closeButtonVisible": true,
+                "title": "exmpleplugin",
+				"text": "exmpleplugin"
+                
+			};
+		}
+
+	});
+});
